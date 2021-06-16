03352558ed7bfdc9dbff72374650db8c44632e20
CoopTRUE <coopdog4545@gmail.com>
2021-06-15 22:35:56 -0700

    get_cipher function for less repeating lines of code

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from cryptography.hazmat.backends import default_backend
 from base64 import urlsafe_b64encode
 from cryptography.fernet import Fernet
 
-
-def raw_encrypt(data, password, salt):
+def get_cipher(password, salt):
     kdf = PBKDF2HMAC(
         algorithm=SHA256(),
         length=32,
@@ -15,19 +14,15 @@ def raw_encrypt(data, password, salt):
     )
     encrypted_password = urlsafe_b64encode(kdf.derive(password))
     cipher_suite = Fernet(encrypted_password)
+    return cipher_suite
+
+def raw_encrypt(data, password, salt):
+    cipher_suite = get_cipher(password, salt)
     encrypted_data = cipher_suite.encrypt(data)
     return encrypted_data
 
 def raw_decrypt(encrypted_data, password, salt):
-    kdf = PBKDF2HMAC(
-        algorithm=SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=default_backend()
-    )
-    encrypted_password = urlsafe_b64encode(kdf.derive(password))
-    cipher_suite = Fernet(encrypted_password)
+    cipher_suite = get_cipher(password, salt)
     decryted_data = cipher_suite.decrypt(encrypted_data)
     return decryted_data
 
